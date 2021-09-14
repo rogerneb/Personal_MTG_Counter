b5672e5ad0f79b0a248dcd4c30d9ff70a827989f
Roger Nebot <roger.neb@gmail.com>
2021-09-14 16:09:22 +0200

    Upgraded dices

--- js/javascript.js
+++ js/javascript.js
@@ -33,21 +33,30 @@ function color_change(color) {
 }
 
 //add or remove life form counter
-function life_change(m){
+function life_change(m) {
   var num = $("#life").text();
   if (m == "+"){num++;}
   else{num--;}
   $("#life").text(num);
 }
 
-function life_reset(){
+function life_reset() {
   var a = confirm("Are you really sure you want to reset the life counter?");
   if (a == true) {
     $("#life").text("20");
   }
 }
 
-function roll_dice(n){
-  var dice_number = Math.floor(Math.random() * n)+1;
-  alert(n+"D: "+dice_number);
+function roll_dice(n) {
+  var dice_number = Math.floor(Math.random() * n)+1; //create random number between 1 and dice max (n)
+  $('#d'+n).html("<span onclick=restore_dice("+n+")>"+dice_number+"</span>");
+}
+
+function restore_dice(n) { //restore the image of dice
+  $('#d'+n).html("<img id=dice"+n+" src=img/dice/"+n+".png alt="+n+"D onclick=roll_dice("+n+")>");
+  if ($("#white").attr("class") == "color_selected_white") { //if selected color for interface is white, then put black dices
+    $('#d'+n).html("<img id=dice"+n+" src=img/dice/"+n+"_black.png alt="+n+"D onclick=roll_dice("+n+")>");
+  }else{ //if not, put black dices
+    $('#d'+n).html("<img id=dice"+n+" src=img/dice/"+n+".png alt="+n+"D onclick=roll_dice("+n+")>");
+  }
 }
