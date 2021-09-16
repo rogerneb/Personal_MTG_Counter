91c7489dd41edabf444514884e2f0c4194721200
roger.neb <roger.neb@gmail.com>
2021-09-16 13:52:24 +0200

    Reset button

--- js/javascript.js
+++ js/javascript.js
@@ -46,6 +46,10 @@ function counter_reset() {
     $("#poison").text("0");
     $("#energy").text("0");
     $("#other").text("0");
+
+    //back to top
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 }
 
